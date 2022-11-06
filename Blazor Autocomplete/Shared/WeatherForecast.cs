72bb9fd1cab93b01e97e41625a9c8ab267d6abef
Mike Brind <mikebrind@stepcomms.com>
2022-11-06 09:13:08 +0000

    Committed

---
+++ Blazor Autocomplete/Shared/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace Blazor_Autocomplete.Shared;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public string? Summary { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
